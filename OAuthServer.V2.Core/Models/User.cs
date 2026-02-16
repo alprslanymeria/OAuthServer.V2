b75869d96681c227e46d39e63e9a633504943bcc
alparslan <alparslanorgnl@gmail.com>
2026-02-16 14:44:01 +0300

    Add User and UserRefreshToken models; update dependencies

---
+++ OAuthServer.V2.Core/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OAuthServer.V2.Core.Models;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; } = null!;
+    public string? Image { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Locale { get; set; } = null!;
+}
---
+++ OAuthServer.V2.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,8 @@
+﻿namespace OAuthServer.V2.Core.Models;
+
+public class UserRefreshToken
+{
+    public string UserId { get; set; } = default!;
+    public string Code { get; set; } = default!;
+    public DateTime Expiration { get; set; }
+}
